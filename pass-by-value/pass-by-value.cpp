5e85580feebbf95f620927c03bc5962f9581948d
Rupesh Narayan <79307533+avid-programmer@users.noreply.github.com>
2026-05-02 12:21:21 +0530

    Added comments. Fixed copy constructor.

--- pass-by-value/pass-by-value.cpp
+++ pass-by-value/pass-by-value.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
+/*
+ * You can see that when passing argument to a function by value,
+ * a local copy of the Object is created, so that function foo can
+ * use it without affecting the original object which was passed.
+ * To create this copy, it invokes the custom copy constructor of
+ * class Object.
+ *
+ * I have created a dummy class Object and provided a custom copy
+ * constructor so that you can see whenever it is called.
+ *
+ */
+
 class Object {
   int data;
 
 public:
   Object(int e) : data{e} {}
 
-  Object(Object &o) { std::cout << "Copy constructor called \n"; }
+  Object(const Object &o) : data{o.data} {
+    std::cout << "Copy constructor called \n";
+  }
 };
 
 void foo(Object o) { std::cout << "foo called\n"; }
